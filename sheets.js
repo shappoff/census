cff9a6a06feca8dcc5c5180927b787cc367d5419
Sergey Shappo <shappoff@gmail.com>
2023-02-12 22:21:46 +0300

    up

--- sheets.js
+++ sheets.js
@@ -48,6 +48,7 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 /**
